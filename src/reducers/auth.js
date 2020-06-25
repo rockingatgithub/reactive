d83e8891f5885bd2f4e2b0ffc1a177fbce36f980
rockingatgithub <contactwithsid@gmail.com>
2020-06-26 02:07:11 +0530

    auth section added

---
+++ src/reducers/auth.js
@@ -0,0 +1,39 @@
+import {
+  LOGIN_START,
+  LOGIN_FAILED,
+  LOGIN_SUCCESS,
+} from '../actions/actionTypes';
+import { bindActionCreators } from 'redux';
+
+const initialAuthState = {
+  user: {},
+  error: null,
+  isLoggedin: false,
+  inProgress: false,
+};
+
+export default function auth(state = initialAuthState, action) {
+  switch (action.type) {
+    case LOGIN_START:
+      return {
+        ...state,
+        inProgress: true,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        user: action.user,
+        inProgress: false,
+        isLoggedin: true,
+        error: null,
+      };
+    case LOGIN_FAILED:
+      return {
+        ...state,
+        inProgress: false,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
+}
